625398096581c73e57e9615f5242164f0055b2b4
whary11 <whary11@gmail.com>
2017-07-12 20:58:23 -0500

    Se separó el footer a un archivo independiente que postariormente será incluido con PHP required_once

--- includes/footer.php
+++ includes/footer.php
@@ -1,143 +1,4 @@
-
-<!DOCTYPE html>
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <title>¿Qué es el Neuromarketing? | Agencia Web Bogotá</title>
-    <meta name="description" content="Nexus is a modern & responsive HTML5 template.">
-    <meta name="viewport" content="width=device-width, initial-scale=0.85">
-    <meta name="HandheldFriendly" content="True">
-    <meta name="MobileOptimized" content="320">
-
-    <!-- Icons & favicons -->
-    <link rel="apple-touch-icon" href="assets/images/apple-icon-touch.png">
-    <link rel="icon" href="assets/images/favicon.png">
-    <!-- Stylesheet -->
-    <link rel="stylesheet" href="assets/styles/css/app.css">
-    <link rel="stylesheet" class="color-switcher" href="assets/styles/css/app.css">
-    
-    <!-- Modernizr -->
-    <script src="assets/js/modernizr.min.js"></script>
-</head>
-<body class="further portfolio single-project">
-
-    <!-- SITE CONTENT -->
-
-    <div id="site-content">
-
-        <!-- Header -->
-
-        <header class="app-header" id="app-header">
-    <div class="container">
-        <div class="header-wrapper">
-            <!-- Logo -->
-            <div class="logo" id="logo">
-                <!-- image logo -->
-                <a href="index-single.html" class="image-logo">
-                    <img src="assets/images/logo-agencia-web-bogota.svg" alt="Logo Agencia Web Bogota" title="Agencia Web Bogota" />
-                </a>
-            </div>
-            <!-- Main-Nav -->
-                            <nav class="main-nav">
-                    <ul>
-                        <li><a href="index.html">Inicio</a></li>
-                        <li><a href="index.html#s-disenoweb">Diseño Web</a></li>
-                        <li><a href="index.html#s-servicios">Servicios Digitales</a></li>
-                        <li><a href="index.html#s-nosotros">Nuestra Agencia</a></li>
-                        <li><a href="index.html#s-planes">Ofertas</a></li>
-                        <li><a href="index.html#s-contacto">Contacto</a></li>
-                    </ul>                    
-                </nav>
-                    </div>
-    </div>
-</header>
-        <!-- Sub-Header -->
-
-        <section class="hero sub-header">
-    <div class="container inactive">
-        <div class="sh-title-wrapper">
-            <h1>¿Que es Neuromarketing?</h1>
-            <p>Antes vender era una técnica, ahora es una ciencia</p>            
-        </div>
-    </div>
-</section>        
-        <!-- Main Content -->
-
-<section class="section primary single-project inactive">
-
-    <div class="container">
-
-        <header class="sep active">
-            <div class="section-title">
-                <h2>Vender <i>sin vender</i></h2>
-                <h3>¿Como funciona?</h3>
-            </div>
-            <p><a>El Neuromarketing</a> es un area de la publicidad que nos enseña <b>como funciona la mente humana en los procesos de venta</b>. Las compras en su mayoría se hacen de forma inconsciente, el consumidor no sabe por qué compra. Implementando conceptos Neuromarketing podemos hacer que la gente elija nuestro producto o servicio sin hacer grandes esfuerzos ni gastar inmensas sumas de dinero en publicidad.</p>
-        </header>
-
-        <hr class="dotted">
-
-        <div class="row info-graphic">
-            <div class="span-4 va-middle blue-media">
-                <img src="http://placehold.it/800x543" alt="" />
-            </div>
-            <div class="span-8 va-middle">
-                <header class="title">
-                    <h4>¿Para quienes funciona?</h4>                
-                    <h5>¿Me sirve para impulsar mi negocio?</h5> 
-                </header>               
-                <p class="lead" align="justify">Los conceptos de Neuromarketing son <b>aplicables para todo tipo de negocio</b>, las ventas son el objetivo común que tenemos todos. Y al tratarse de estrategias publicitarias que buscan aumentar las conversiones, crear un mayor impacto entre la gente y mejor recordación de las marcas, es mas que obvio que las podemos aplicar a nuestro caso particular sin excepción.</p>
-            </div>
-        </div>
-        <hr class="dotted">
-        <header class="sep active">
-            <div class="section-title">
-                <h2>¿Como aplicamos <a href="portfolio.html">Neuromarketing</a> en nuestros servicios?</h2>                
-            </div>
-            <p>La optmización visual es <a>nuestro fuerte</a>. Los colores, los textos, las imágenes y la orientación de los objetos, son algunos de los aspectos que logramos optimizar para que su producto o servicio se situe por encima de su competencia</p>
-        </header>
-
-        <div class="project-carousel">
-            <div class="project-preview">
-                <img src="assets/images/desktop-off.png" alt="Single Project" />
-                <div class="owl-carousel previews" id="project-gallery">
-                    <a href="http://placehold.it/800x543">
-                        <img src="http://placehold.it/800x543" alt="" />
-                    </a>
-                    <a href="http://placehold.it/800x543">
-                        <img src="http://placehold.it/800x543" alt="" />
-                    </a>
-                    <a href="http://placehold.it/800x543">
-                        <img src="http://placehold.it/800x543" alt="" />
-                    </a>
-                </div>
-            </div>
-            <div class="nav-carousel">
-                <div class="icon-round-border-lrg nav-prev">
-                    <i class="fa fa-angle-left"></i>
-                </div>
-                <div class="icon-round-border-lrg nav-next">
-                    <i class="fa fa-angle-right"></i>
-                </div>
-            </div>
-        </div>
-    </div>
-
-</section>
-<section class="section banner alt">
-    <div class="container">
-        <div class="content">
-             <p>¿Tiene dudas sobrer como implementar el <a>Neuromarketing</a> en su negocio?</p>
-            <h4>Le asesoramos <b>sin costo</b> y <i>sin compromisos</i></h4>            
-        </div>
-        <div class="meta">
-            <a href="index.html#s-contacto" class="button brand-1 round">Contáctenos</a>            
-        </div>
-    </div>
-</section>    
- <!-- Footer -->
-
-        <footer class="app-footer">
+ <footer class="app-footer">
     <div class="container footer-content">
         <div class="row">
             <div class="span-3 footer-col">
@@ -151,7 +12,7 @@
                 <ul>
                     <li><b><i class="fa fa-globe"></i> Oficina:</b> Calle 2 No. 91C-85 Bogotá</li>
                     <li><b><i class="fa fa-phone-square"></i> Teléfono:</b> 316 878 56 01</li>
-                    <li><b><i class="fa fa-envelope"></i> Email:</b> <a href="mailto:support@themeforest.net" target="_blank">info@agenciawebbogota.com</a></li>
+                    <li><b><i class="fa fa-envelope"></i> Email:</b> <a href="mailto:support@themeforest.net">info@agenciawebbogota.com</a></li>
                 </ul>
                 <br />
                 <div class="social-icons">                    
@@ -173,11 +34,12 @@
             <div class="span-3 footer-col">
                 <h3>Reciba</h3>
                 <h4>Tips de Neuroventas</h4>
-                <p>Le compartiremos algunos concejos rápidos que le ayudarán a llegar de forma más efectiva a sus clientes</p>
-                <form class="newsletter h5-valid">
+                <!-- <p>Le compartiremos algunos concejos rápidos que le ayudarán a llegar de forma más efectiva a sus clientes</p> -->
+                <p id="resultado-sus"></p>
+                <form class="newsletter h5-valid" id="suscribirse">
                     <div class="form-element">
-                        <input type="email" class="box" required="" placeholder="Ingrese su correo electrónico" />
-                        <label>Correo Electrónico</label>
+                        <input type="email" class="box" required="" placeholder="Ingrese su correo electrónico" name="correo-suscribe" />
+                        <label >Correo Electrónico</label>
                         <button type="submit">
                             <i class="fa fa-check"></i>
                         </button>
@@ -280,27 +142,27 @@
                 <nav class="footer-nav">                
                     <ul>
                         <li>
-                            <a href="index.html#s-nosotros">
+                            <a href="#s-nosotros">
                                 Agencia
                             </a>
                         </li>
                         <li>
-                            <a href="index.html#s-servicios">
+                            <a href="#s-servicios">
                                 Servicios
                             </a>
                         </li>
                         <li>
-                            <a href="index.html#s-disenoweb">
+                            <a href="#s-disenoweb">
                                 Diseño Web
                             </a>
                         </li>
                         <li>
-                            <a href="index.html#s-planes">
+                            <a href="#s-planes">
                                 Planes
                             </a>
                         </li>
                         <li>
-                            <a href="index.html#s-contacto">
+                            <a href="#s-contacto">
                                 Contacto
                             </a>
                         </li>
@@ -311,7 +173,6 @@
     </div>
 </footer>
     </div><!-- end #site-content -->
-
 
     <!-- TOOLS/UTILITIES -->
 
@@ -344,6 +205,8 @@
     <i class="fa fa-angle-up"></i>
 </a>
 
+
+
 <!-- Site Overlay -->
 <div class="site-overlay invisible" id="site-overlay"></div>
 
@@ -360,11 +223,8 @@
 <!--Plugins -->
 <script src="assets/js/owl-carousel.min.js"></script>
 <script src="assets/js/magnific-popup.min.js"></script>    
-
-    <!-- General -->
-    
     <script src="assets/js/app.js"></script>
-
+    <script type="text/javascript" src="assets/js/form-registro.js"></script>
+    <script src="assets/js/homepage.js"></script>
 </body>
-
 </html>
---
+++ neuromarketing.php
@@ -0,0 +1,142 @@
+
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <title>¿Qué es el Neuromarketing? | Agencia Web Bogotá</title>
+    <meta name="description" content="Nexus is a modern & responsive HTML5 template.">
+    <meta name="viewport" content="width=device-width, initial-scale=0.85">
+    <meta name="HandheldFriendly" content="True">
+    <meta name="MobileOptimized" content="320">
+
+    <!-- Icons & favicons -->
+    <link rel="apple-touch-icon" href="assets/images/apple-icon-touch.png">
+    <link rel="icon" href="assets/images/favicon.png">
+    <!-- Stylesheet -->
+    <link rel="stylesheet" href="assets/styles/css/app.css">
+    <link rel="stylesheet" class="color-switcher" href="assets/styles/css/app.css">
+    
+    <!-- Modernizr -->
+    <script src="assets/js/modernizr.min.js"></script>
+</head>
+<body class="further portfolio single-project">
+
+    <!-- SITE CONTENT -->
+
+    <div id="site-content">
+
+        <!-- Header -->
+
+        <header class="app-header" id="app-header">
+    <div class="container">
+        <div class="header-wrapper">
+            <!-- Logo -->
+            <div class="logo" id="logo">
+                <!-- image logo -->
+                <a href="index-single.html" class="image-logo">
+                    <img src="assets/images/logo-agencia-web-bogota.svg" alt="Logo Agencia Web Bogota" title="Agencia Web Bogota" />
+                </a>
+            </div>
+            <!-- Main-Nav -->
+                            <nav class="main-nav">
+                    <ul>
+                        <li><a href="index.html">Inicio</a></li>
+                        <li><a href="index.html#s-disenoweb">Diseño Web</a></li>
+                        <li><a href="index.html#s-servicios">Servicios Digitales</a></li>
+                        <li><a href="index.html#s-nosotros">Nuestra Agencia</a></li>
+                        <li><a href="index.html#s-planes">Ofertas</a></li>
+                        <li><a href="index.html#s-contacto">Contacto</a></li>
+                    </ul>                    
+                </nav>
+                    </div>
+    </div>
+</header>
+        <!-- Sub-Header -->
+
+        <section class="hero sub-header">
+    <div class="container inactive">
+        <div class="sh-title-wrapper">
+            <h1>¿Que es Neuromarketing?</h1>
+            <p>Antes vender era una técnica, ahora es una ciencia</p>            
+        </div>
+    </div>
+</section>        
+        <!-- Main Content -->
+
+<section class="section primary single-project inactive">
+
+    <div class="container">
+
+        <header class="sep active">
+            <div class="section-title">
+                <h2>Vender <i>sin vender</i></h2>
+                <h3>¿Como funciona?</h3>
+            </div>
+            <p><a>El Neuromarketing</a> es un area de la publicidad que nos enseña <b>como funciona la mente humana en los procesos de venta</b>. Las compras en su mayoría se hacen de forma inconsciente, el consumidor no sabe por qué compra. Implementando conceptos Neuromarketing podemos hacer que la gente elija nuestro producto o servicio sin hacer grandes esfuerzos ni gastar inmensas sumas de dinero en publicidad.</p>
+        </header>
+
+        <hr class="dotted">
+
+        <div class="row info-graphic">
+            <div class="span-4 va-middle blue-media">
+                <img src="http://placehold.it/800x543" alt="" />
+            </div>
+            <div class="span-8 va-middle">
+                <header class="title">
+                    <h4>¿Para quienes funciona?</h4>                
+                    <h5>¿Me sirve para impulsar mi negocio?</h5> 
+                </header>               
+                <p class="lead" align="justify">Los conceptos de Neuromarketing son <b>aplicables para todo tipo de negocio</b>, las ventas son el objetivo común que tenemos todos. Y al tratarse de estrategias publicitarias que buscan aumentar las conversiones, crear un mayor impacto entre la gente y mejor recordación de las marcas, es mas que obvio que las podemos aplicar a nuestro caso particular sin excepción.</p>
+            </div>
+        </div>
+        <hr class="dotted">
+        <header class="sep active">
+            <div class="section-title">
+                <h2>¿Como aplicamos <a href="portfolio.html">Neuromarketing</a> en nuestros servicios?</h2>                
+            </div>
+            <p>La optmización visual es <a>nuestro fuerte</a>. Los colores, los textos, las imágenes y la orientación de los objetos, son algunos de los aspectos que logramos optimizar para que su producto o servicio se situe por encima de su competencia</p>
+        </header>
+
+        <div class="project-carousel">
+            <div class="project-preview">
+                <img src="assets/images/desktop-off.png" alt="Single Project" />
+                <div class="owl-carousel previews" id="project-gallery">
+                    <a href="http://placehold.it/800x543">
+                        <img src="http://placehold.it/800x543" alt="" />
+                    </a>
+                    <a href="http://placehold.it/800x543">
+                        <img src="http://placehold.it/800x543" alt="" />
+                    </a>
+                    <a href="http://placehold.it/800x543">
+                        <img src="http://placehold.it/800x543" alt="" />
+                    </a>
+                </div>
+            </div>
+            <div class="nav-carousel">
+                <div class="icon-round-border-lrg nav-prev">
+                    <i class="fa fa-angle-left"></i>
+                </div>
+                <div class="icon-round-border-lrg nav-next">
+                    <i class="fa fa-angle-right"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</section>
+<section class="section banner alt">
+    <div class="container">
+        <div class="content">
+             <p>¿Tiene dudas sobrer como implementar el <a>Neuromarketing</a> en su negocio?</p>
+            <h4>Le asesoramos <b>sin costo</b> y <i>sin compromisos</i></h4>            
+        </div>
+        <div class="meta">
+            <a href="index.html#s-contacto" class="button brand-1 round">Contáctenos</a>            
+        </div>
+    </div>
+</section>    
+ <!-- Footer -->
+
+ <?php 
+    require_once 'includes/footer.php';
+  ?>
